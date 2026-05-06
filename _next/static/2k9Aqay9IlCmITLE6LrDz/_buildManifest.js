5b587bb934dee8f44f512bd543d592690ac725aa
masoud-nemati <132232813+masoud-nemati@users.noreply.github.com>
2026-05-07 00:48:23 +0300

    Updates

--- _next/static/2k9Aqay9IlCmITLE6LrDz/_buildManifest.js
+++ _next/static/2k9Aqay9IlCmITLE6LrDz/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/masoud-site//_next/:path+",
+        "destination": "/masoud-site/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [

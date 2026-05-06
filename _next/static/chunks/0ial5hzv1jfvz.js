b85f0c9e796fcdccf3cc0ad8820b0d54413a85a6
masoud-nemati <132232813+masoud-nemati@users.noreply.github.com>
2026-05-07 00:26:44 +0300

    Updates

---
+++ _next/static/chunks/0ial5hzv1jfvz.js
@@ -0,0 +1,13 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,45748,l=>{l.v({h2skill:"skills-module__qeCuQW__h2skill",meter:"skills-module__qeCuQW__meter",sectionTitle:"skills-module__qeCuQW__sectionTitle",skillCard:"skills-module__qeCuQW__skillCard",skillLevel:"skills-module__qeCuQW__skillLevel",skillLogo:"skills-module__qeCuQW__skillLogo",skillName:"skills-module__qeCuQW__skillName",skillsList:"skills-module__qeCuQW__skillsList",skillsPage:"skills-module__qeCuQW__skillsPage"})},76626,l=>{"use strict";var s=l.i(43476),e=l.i(57688),a=l.i(45748);l.s(["default",0,function({name:l,level:i,logo:t}){return(0,s.jsxs)("div",{className:`${a.default.skillCard}
+   bg-zinc-900 border-slate-200 text-slate-900
+    dark:bg-slate-950 dark:border-slate-800 dark:text-slate-100
+    hover:shadow-lg
+    dark:hover:shadow-[0_20px_50px_rgba(56,189,248,0.15)]
+  `,children:[(0,s.jsx)("div",{className:`${a.default.skillLogo}
+      bg-sky-500/10
+      dark:bg-sky-400/10
+    `,children:(0,s.jsx)(e.default,{src:t,alt:`${l} logo`,width:32,height:32})}),(0,s.jsx)("span",{className:a.default.skillName,children:l}),(0,s.jsx)("meter",{className:`${a.default.meter}
+      bg-slate-200 dark:bg-slate-800
+    `,value:i,min:0,max:100}),(0,s.jsxs)("span",{className:`${a.default.skillLevel}
+      text-slate-600 dark:text-slate-400
+    `,children:[i,"%"]})]})}])}]);
